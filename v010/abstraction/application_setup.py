9796132f1d4d83ebb29bfc597c7aa599b02a4e86
Jakub Porębski <kuba.porebski3@gmail.com>
2025-10-18 14:42:43 +0200

    feat: start 0.1.0

---
+++ v010/abstraction/application_setup.py
@@ -0,0 +1,54 @@
+from abc import ABC
+from dataclasses import dataclass
+from typing import Optional, Dict, List
+
+from v010.abstraction.database_service import DatabaseService
+from v010.abstraction.embedding_service import EmbeddingService
+from v010.abstraction.retriever_service import RetrieverService
+
+@dataclass
+class ServiceConfig:
+    model_name: str
+    api_key: str
+
+@dataclass
+class DatabaseConfig:
+    connection_string: str
+    api_key: str
+
+@dataclass
+class ApplicationConfig:
+    default_cfg: ServiceConfig
+    database_cfg: DatabaseConfig
+    retriever_cfg: Optional[ServiceConfig] = None
+    embedder_cfg: Optional[ServiceConfig] = None
+
+ApplicationSetupInstance = Dict[str, object]
+
+class ApplicationSetup(ABC):
+    _retriever: RetrieverService = None
+    _embedding: EmbeddingService = None
+    _database: DatabaseService = None
+    _config: ApplicationConfig = None
+
+    def __init__(self,
+                 retriever: RetrieverService,
+                 embedder: EmbeddingService,
+                 database: DatabaseService,
+                 config: ApplicationConfig):
+        self._retriever = retriever
+        self._embedder = embedder
+        self._database = database
+        self._config = config
+
+    @classmethod
+    def index_documents(cls, documents: List[str]) -> List[List[float]]:
+        if not cls._embedder:
+            raise Exception("Embedding service not configured")
+        return cls._embedder.embed(documents)
+
+    @classmethod
+    def send_query(cls, query: str) -> str:
+        if not cls._retriever:
+            raise Exception("Retriever service not configured")
+        return cls._retriever.retrieve(query)
